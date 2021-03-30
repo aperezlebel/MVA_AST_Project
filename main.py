09b2dab371eb291e9903d15988f7f8206bc15f9a
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2021-03-30 13:54:18 +0200

    Add method

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import src.benchmark as benchmark
 parser = argparse.ArgumentParser(description='MVA RP Data Challenge 2021')
 parser.add_argument('script', type=str, help='The subscript to run.')
 parser.add_argument('action', type=str, help='The action to run.')
+parser.add_argument('--m', type=str, default='dl', help='The method to use.')
 parser.add_argument('--ds', type=str, default='btc', help='The dataset.')
 parser.add_argument('--w', type=int, default=24, help='Windows width.')
 parser.add_argument('--s', type=int, default=12, help='Windows stride.')
