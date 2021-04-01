9aa4426aa1b9df64f3a85b8a6d1cc514457696da
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2021-04-01 23:53:11 +0200

    Update experiments

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ parser.add_argument('--m', type=str, default='dl', help='The method to use.')
 parser.add_argument('--ds', type=str, default='btc', help='The dataset.')
 parser.add_argument('--w', type=int, default=7, help='Windows width.')
 parser.add_argument('--s', type=int, default=7, help='Windows stride.')
-parser.add_argument('--splits', type=int, default=2, help='Number of splits in the crossvalidation.')
+parser.add_argument('--splits', type=int, default=5, help='Number of splits in the crossvalidation.')
 parser.add_argument('--dist', type=str, default='rmsre', help='Distance to use.')
 parser.add_argument('--iter', type=int, default=10, help='Number of iterations.')
+parser.add_argument('--atoms', type=int, default=5, help='Number of atoms.')
 
 args = parser.parse_args()
 
